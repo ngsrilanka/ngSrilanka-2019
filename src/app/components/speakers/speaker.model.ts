c8f812628d66f48dcfd20e9e021edfb52fee9aec
azkarmoulana <azkar.moulana@gmail.com>
2019-07-15 13:41:58 +0530

    optimizing site - replacing with optimized webp

--- src/app/components/speakers/speaker.model.ts
+++ src/app/components/speakers/speaker.model.ts
@@ -1,3 +1,3 @@
 export class Speaker {
-    constructor (public name: string, public company: string, public twitter: string, public github: string, public imagePath: string){}
+    constructor (public name: string, public company: string, public twitter: string, public github: string, public imagePathJpg: string, public imagePathWebp: string){}
 }
