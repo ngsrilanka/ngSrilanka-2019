bbbc13c58b231318286104b330f561b4e9cec484
thivatm <thivagartm@gmail.com>
2019-07-19 12:57:12 +0530

    design changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { ComingSoonPageComponent } from './components/coming-soon-page/coming-soon-page.component';
-import { MailchimpFormComponent } from './components/mailchimp-form/mailchimp-form.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { SocialIconsComponentComponent } from './components/common/social-icons-component/social-icons-component.component';
-import { FormsModule } from '@angular/forms';
-import { AboutComponent } from './components/about/about.component';
-import { SpeakersComponent } from './components/speakers/speakers.component';
-import { SponsorsComponent } from './components/sponsors/sponsors.component';
-import { VenueComponent } from './components/venue/venue.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { TicketsComponent } from './components/tickets/tickets.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavComponent } from './components/common/nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { ComingSoonPageComponent } from "./components/coming-soon-page/coming-soon-page.component";
+import { MailchimpFormComponent } from "./components/mailchimp-form/mailchimp-form.component";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+import { SocialIconsComponentComponent } from "./components/common/social-icons-component/social-icons-component.component";
+import { FormsModule } from "@angular/forms";
+import { AboutComponent } from "./components/about/about.component";
+import { SpeakersComponent } from "./components/speakers/speakers.component";
+import { SponsorsComponent } from "./components/sponsors/sponsors.component";
+import { VenueComponent } from "./components/venue/venue.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { TicketsComponent } from "./components/tickets/tickets.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NavComponent } from "./components/common/nav/nav.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "landing", component: LandingPageComponent },
+  { path: "", redirectTo: "landing", pathMatch: "full" }
+];
 
 @NgModule({
   declarations: [
@@ -37,10 +43,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule
-    
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
