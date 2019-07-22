4632c0dfee002af4230cc76d0bc7de7b5a52f8d2
thivatm <thivagartm@gmail.com>
2019-07-22 17:15:14 +0530

    fixed router issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, Component } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ComingSoonPageComponent } from "./components/coming-soon-page/coming-soon-page.component";
